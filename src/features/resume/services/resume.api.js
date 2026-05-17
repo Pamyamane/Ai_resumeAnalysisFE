4323952ed992770a4b9e89b703f5acaa22300a6b
Pamyamane <pramodmane043@gmail.com>
2026-05-17 20:46:24 +0530

    rioute

--- src/features/resume/services/resume.api.js
+++ src/features/resume/services/resume.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-resume-analysis-be.vercel.app",
+  baseURL: "https://ai-resume-analysis-be.vercel.app/api",
   withCredentials: true,
 });
 
